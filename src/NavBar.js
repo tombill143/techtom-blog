e363abeadb16f641c4c1c50c9506feecc749ee88
tom <tom@twentyfour.dk>
2024-09-12 15:15:23 +0200

    Finished project

---
+++ src/NavBar.js
@@ -0,0 +1,16 @@
+import {Link} from "react-router-dom"
+
+const NavBar = () => {
+    return ( 
+        <nav className="navbar">
+        <h1>The Techtom Blog</h1>
+        <div className="links">
+            <Link to="/">Home</Link>
+            <Link to="/create">New Blog</Link>
+        </div>
+        </nav>
+     );
+}
+ 
+
+export default NavBar;
